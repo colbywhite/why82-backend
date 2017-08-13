40c3ac1e92fc89c43f40cdad3ea7ae68cf5e2f6f
Colby M. White <colbywhite@users.noreply.github.com>
2017-08-13 12:01:24 -0500

    include date in file name

--- handler.js
+++ handler.js
@@ -2,9 +2,17 @@
 
 const client = require('./lib/msf_client')
 const s3 = require('./lib/s3_client')
+const moment = require('moment-timezone')
+
+const buildFileName = (date) => {
+  if(!date) {
+    date = moment()
+  }
+  return date.tz('America/New_York').format('YYYY-MM-DD-[stats.json]')
+}
 
 module.exports.save_stats = (event, context, callback) => {
-  const save = s3.save.bind(s3, 'stats.json')
+  const save = s3.save.bind(s3, buildFileName())
   client.getTeamStats()
     .then(JSON.stringify)
     .then(save)
