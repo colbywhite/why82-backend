b64c74fbd0869d9cbf368d13f59e54901c9dd23b
Colby M. White <colbywhite@users.noreply.github.com>
2017-08-13 11:42:08 -0500

    save resulting stats to s3

--- handler.js
+++ handler.js
@@ -1,15 +1,19 @@
 'use strict';
 
 const client = require('./lib/msf_client')
+const s3 = require('./lib/s3_client')
 
 module.exports.save_stats = (event, context, callback) => {
+  const save = s3.save.bind(s3, 'stats.json')
   client.getTeamStats()
+    .then(JSON.stringify)
+    .then(save)
     .then((stats) => {
       const response = {
         statusCode: 200,
         body: JSON.stringify(stats)
-      };
-      callback(null, response);
+      }
+      callback(null, response)
     })
     .catch(console.error)
 };
