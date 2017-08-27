9efba06a80341745eb2b688fca3f9061e3f7cfe8
Colby M. White <colbywhite@users.noreply.github.com>
2017-08-26 20:55:11 -0500

    rename certain vars/filenames

--- save_stats.js
+++ save_stats.js
@@ -13,17 +13,18 @@ const buildFileName = (date, postfix) => {
 }
 
 module.exports.save_stats = (event, context, callback) => {
-  const now = moment()
-  const save_stats = s3.save_json.bind(s3, buildFileName(now, 'stats'))
-  const save_sked = s3.save_json.bind(s3, buildFileName(now, 'schedule'))
-  const save_tiers = s3.save_json.bind(s3, buildFileName(now, 'tiers'))
+  const now = moment('20170216', "YYYYMMDD")
+  const save_stats = s3.save_json.bind(s3, buildFileName(now, 'raw.stats'))
+  const save_sked = s3.save_json.bind(s3, buildFileName(now, 'raw.schedule'))
+  const save_team_scores = s3.save_json.bind(s3, buildFileName(now, 'team.scores'))
   const get_sked = client.getMultiDaySchedule.bind(client, now, 8)
   let getTeamScores = client.getTeamStats()
     .then(save_stats)
     .then(team_calc.calc)
-    .then(save_tiers)
+    .then(save_team_scores)
   let getSchedule = client.getMultiDaySchedule(now, 8)
     .then(save_sked)
+
   Promise.all([getTeamScores, getSchedule])
     .then((results) => {
       const response = {
--- test/team_calculator.spec.js
+++ test/team_calculator.spec.js
@@ -3,40 +3,41 @@ const stats = require('./resources/2017-08-13-stats')
 const team_calculator = require('../lib/team_calculator')
 
 describe('team_calculator', () => {
-  let tiers
+  let scores
+
   before(() => {
-    tiers = team_calculator.calc(stats)
+    scores = team_calculator.calc(stats)
   })
 
   it('keeps team information', () => {
-    assert.strictEqual(tiers.GSW.abbreviated_name, 'GSW')
-    assert.strictEqual(tiers.GSW.short_name, 'Warriors')
-    assert.strictEqual(tiers.GSW.full_name, 'Golden State Warriors')
+    assert.strictEqual(scores.GSW.abbreviated_name, 'GSW')
+    assert.strictEqual(scores.GSW.short_name, 'Warriors')
+    assert.strictEqual(scores.GSW.full_name, 'Golden State Warriors')
   })
 
   it('calculates win_loss scores', () => {
-    assert.strictEqual(tiers.GSW.metrics.win_loss.score, 8)
-    assert.strictEqual(tiers.WAS.metrics.win_loss.score, 3)
-    assert.strictEqual(tiers.LAL.metrics.win_loss.score, 0)
-    assert.strictEqual(tiers.BRO.metrics.win_loss.score, -1)
+    assert.strictEqual(scores.GSW.metrics.win_loss.score, 8)
+    assert.strictEqual(scores.WAS.metrics.win_loss.score, 3)
+    assert.strictEqual(scores.LAL.metrics.win_loss.score, 0)
+    assert.strictEqual(scores.BRO.metrics.win_loss.score, -1)
   })
 
   it('calculates offensive scores', () => {
-    assert.strictEqual(tiers.GSW.metrics.offense.score, 21)
-    assert.strictEqual(tiers.WAS.metrics.offense.score, 5)
-    assert.strictEqual(tiers.LAL.metrics.offense.score, 2)
-    assert.strictEqual(tiers.DAL.metrics.offense.score, -2)
+    assert.strictEqual(scores.GSW.metrics.offense.score, 21)
+    assert.strictEqual(scores.WAS.metrics.offense.score, 5)
+    assert.strictEqual(scores.LAL.metrics.offense.score, 2)
+    assert.strictEqual(scores.DAL.metrics.offense.score, -2)
   })
 
-  it('calculates point difference tiers', () => {
-    assert.strictEqual(tiers.GSW.metrics.pt_diff.score, 13)
-    assert.strictEqual(tiers.WAS.metrics.pt_diff.score, 1)
-    assert.strictEqual(tiers.LAL.metrics.pt_diff.score, -3)
+  it('calculates point difference scores', () => {
+    assert.strictEqual(scores.GSW.metrics.pt_diff.score, 13)
+    assert.strictEqual(scores.WAS.metrics.pt_diff.score, 1)
+    assert.strictEqual(scores.LAL.metrics.pt_diff.score, -3)
   })
 
   it('calculates overall scores', () => {
-    assert.strictEqual(tiers.GSW.overall, 42)
-    assert.strictEqual(tiers.WAS.overall, 9)
-    assert.strictEqual(tiers.LAL.overall, -1)
+    assert.strictEqual(scores.GSW.score, 42)
+    assert.strictEqual(scores.WAS.score, 9)
+    assert.strictEqual(scores.LAL.score, -1)
   })
 })
