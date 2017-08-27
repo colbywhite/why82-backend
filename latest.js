efb868d40219b3f4d552e32c9e11f9492b8751d8
Colby M. White <colbywhite@users.noreply.github.com>
2017-08-27 01:12:38 -0500

    add endpoint to retrive latest teams

--- latest.js
+++ latest.js
@@ -1,15 +1,29 @@
 'use strict';
 const s3 = require('./lib/s3_client')
 
-module.exports.schedule = (event, context, callback) => {
-  const season = parseInt(event.pathParameters.season)
-  s3.readLatest(season + '/', 'game.scores')
+const HEADERS = {
+  "Access-Control-Allow-Origin" : "*",
+  "Access-Control-Allow-Credentials" : true
+}
+
+const get_latest = (season, type, callback) => {
+  return s3.readLatest(season + '/', type)
     .then((result) => {
-      const response = {
+      const response =  {
+        headers: HEADERS,
         statusCode: 200,
         body: result,
       }
-      callback(null, response);
+      callback(null, response)
     })
-    .catch(console.error)
+}
+
+module.exports.schedule = (event, context, callback) => {
+  const season = parseInt(event.pathParameters.season)
+  get_latest(season, 'game.scores', callback).catch(console.error)
+}
+
+module.exports.teams = (event, context, callback) => {
+  const season = parseInt(event.pathParameters.season)
+  get_latest(season, 'team.scores', callback).catch(console.error)
 }
