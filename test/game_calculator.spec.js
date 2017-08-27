1d1b0b2ccfe6672a90dcfc5f44f5ee47a370e0ef
Colby M. White <colbywhite@users.noreply.github.com>
2017-08-27 16:24:37 -0500

    go back to the tier system for grading the actual games

--- test/game_calculator.spec.js
+++ test/game_calculator.spec.js
@@ -7,11 +7,17 @@ describe('game_calculator', () => {
   let graded_sked
   let was_ind
   let bos_chi
+  let lac_gsw
+  let tor_okc
+  let tor_gsw
 
   beforeEach(() => {
     graded_sked = game_calculator.score(team_scores, raw_schedule)
     was_ind = graded_sked['2017-02-16'][0]
     bos_chi = graded_sked['2017-02-16'][1]
+    lac_gsw = graded_sked['2017-02-23'][4]
+    tor_okc = graded_sked['2017-02-24'][0]
+    tor_gsw = graded_sked['2017-02-24'][1]
   })
 
   it('returns games for same dates', () => {
@@ -19,8 +25,11 @@ describe('game_calculator', () => {
   })
 
   it('grades games properly', () => {
-    assert.strictEqual(was_ind.score, 6.5)
-    assert.strictEqual(bos_chi.score, 7)
+    assert.strictEqual(was_ind.score, 'D')
+    assert.strictEqual(bos_chi.score, 'D')
+    assert.strictEqual(tor_okc.score, 'C')
+    assert.strictEqual(tor_gsw.score, 'B')
+    assert.strictEqual(lac_gsw.score, 'A')
   })
 
   it('includes appropriate game time', () => {
