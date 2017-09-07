81cd79f48e6af9479481f48fc47d24f4fcce42c6
Colby M. White <colbywhite@users.noreply.github.com>
2017-09-06 19:11:30 -0500

    remove hardcoded date; ensure date in season

--- save_stats.js
+++ save_stats.js
@@ -6,6 +6,7 @@ const team_calc = require('./lib/team_calculator')
 const game_calc = require('./lib/game_calculator')
 const moment = require('moment-timezone')
 const SEASON = parseInt(process.env.SEASON)
+const EASTERN_TIMEZONE = 'America/New_York'
 
 const buildFileName = (season, date, postfix) => {
   if(!date) {
@@ -15,18 +16,33 @@ const buildFileName = (season, date, postfix) => {
   return `${season}/${filename}`
 }
 
+const getDate = () => {
+ const now = moment.tz(EASTERN_TIMEZONE)
+ const season_info = require(`./lib/seasons/${SEASON}.json`)
+ const start = moment.tz(season_info.start, 'YYYY-MM-DD', EASTERN_TIMEZONE)
+ const end = moment.tz(season_info.end, 'YYYY-MM-DD', EASTERN_TIMEZONE)
+ if (now.isBefore(start)) {
+   return start
+ } else if (now.isAfter(end)) {
+   return end
+ } else {
+   return now
+ }
+}
+
 module.exports.save_stats = (event, context, callback) => {
-  const now = moment.tz('20170216', 'YYYYMMDD', 'America/New_York')
-  const save_stats = s3.save_json.bind(s3, buildFileName(SEASON, now, 'raw.stats'))
-  const save_sked = s3.save_json.bind(s3, buildFileName(SEASON, now, 'raw.schedule'))
-  const save_team_scores = s3.save_json.bind(s3, buildFileName(SEASON, now, 'team.scores'))
-  const save_game_scores = s3.save_json.bind(s3, buildFileName(SEASON, now, 'game.scores'))
-  const get_sked = client.getMultiDaySchedule.bind(client, now, 8)
+  const date = getDate()
+  console.log(`Saving stats for ${date.format("MMM Do YYYY ZZ")}`)
+  const save_stats = s3.save_json.bind(s3, buildFileName(SEASON, date, 'raw.stats'))
+  const save_sked = s3.save_json.bind(s3, buildFileName(SEASON, date, 'raw.schedule'))
+  const save_team_scores = s3.save_json.bind(s3, buildFileName(SEASON, date, 'team.scores'))
+  const save_game_scores = s3.save_json.bind(s3, buildFileName(SEASON, date, 'game.scores'))
+  const get_sked = client.getMultiDaySchedule.bind(client, date, 8)
   let getTeamScores = client.getTeamStats()
     .then(save_stats)
     .then(team_calc.calc)
     .then(save_team_scores)
-  let getSchedule = client.getMultiDaySchedule(now, 8)
+  let getSchedule = client.getMultiDaySchedule(date, 8)
     .then(save_sked)
 
   Promise.all([getTeamScores, getSchedule])
